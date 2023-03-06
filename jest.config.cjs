fd1e455e24a49fcb7524842c48f818746b2cd906
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-06 19:24:29 +0100

    Add sonarcloud coverage configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,15 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/react-app-env.d.ts",
+    "!src/setupTests.ts",
+    "!src/server/index.ts",
+    "!src/server/startServer.ts",
+    "!src/loadEnvironments.ts",
+    "!src/database/connectDatabase.ts",
+    "!src/server/cors.ts",
+  ],
 };
