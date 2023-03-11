f70338a347e36c71cfd9cae11f6030666fabed5c
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-11 19:11:44 +0100

    Add front local origin to cors allowedOrigins

--- src/server/cors.ts
+++ src/server/cors.ts
@@ -4,6 +4,7 @@ import type cors from "cors";
 const allowedOrigins = [
   `${process.env.LOCAL_ORIGIN_URL!}`,
   `${process.env.DEPLOY_ORIGIN_URL!}`,
+  `${process.env.FRONT_LOCAL_ORIGIN_URL!}`,
 ];
 
 const options: cors.CorsOptions = {
