e89cca9b77e33afda6c8968e176bfa8c75ec5e99
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-18 20:38:21 +0100

    Create createMatch controller

--- src/server/controllers/matchesControllers/matchesControllers.ts
+++ src/server/controllers/matchesControllers/matchesControllers.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../../CustomError/CustomError.js";
 import Match from "../../../database/models/Match.js";
+import { type MatchStructure } from "../../../types.js";
 
 export const getMatches = async (
   req: Request,
@@ -40,6 +41,32 @@ export const deleteMatchesById = async (
       (error as Error).message,
       500,
       "Couldn't delete the match"
+    );
+
+    next(customError);
+  }
+};
+
+export const createMatch = async (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    MatchStructure
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const newMatch = req.body;
+
+    await Match.create({ newMatch });
+
+    res.status(201).json({ newMatch });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Couldn't create the match"
     );
 
     next(customError);
